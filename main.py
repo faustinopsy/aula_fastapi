453d09bd2b7e49f8c7a21562b537622a3cfef956
faustinopsy <rodrigohipnose@gmail.com>
2025-03-19 11:54:27 -0300

    parte 1

--- main.py
+++ main.py
@@ -10,7 +10,17 @@ def read_root():
 def read_item(item_id: int, q: Union[str, None] = None):
     return {"item_id": item_id, "q": q}
 
+@app.post("/items")
+async def create_item(item: dict):
+    return {"message": "Recurso criado", "item": item}
 
+@app.put("/items/{item_id}")
+async def update_item(item_id: int, item: dict):
+    return {"message": f"Recurso {item_id} atualizado", "item": item}
+
+@app.delete("/items/{item_id}")
+async def delete_item(item_id: int):
+    return {"message": f"Recurso {item_id} removido"}
 
 if __name__ == "__main__":
     import uvicorn
